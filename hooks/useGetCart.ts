2a6676e1c6753942c91220026f109f414e98351b
Mike <mikemenos95@gmail.com>
2025-12-19 14:58:09 +0200

    fix

--- hooks/useGetCart.ts
+++ hooks/useGetCart.ts
@@ -1,6 +1,8 @@
+import { buildFirstBasketKeyPayload } from "./../lib/utils";
 import { api } from "@/lib/api";
 import { useQuery } from "@tanstack/react-query";
 import type { ICart } from "@/lib/interfaces";
+import { appStore } from "@/stores/appStore";
 
 export function useGetCart({
   trdr,
@@ -9,13 +11,16 @@ export function useGetCart({
   trdr?: string;
   branch?: string;
 }) {
+  const { basketId } = appStore();
+  console.log(basketId);
   return useQuery<ICart, Error>({
-    queryKey: ["cart", branch],
+    queryKey: ["cart", branch, basketId],
     queryFn: async () => {
       const { data } = await api.post("/get-cart", { trdr, branch });
       return data;
     },
     staleTime: 200,
+
     enabled: Boolean(trdr && branch),
   });
 }
